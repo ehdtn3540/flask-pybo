79a7c162ef2e0f43387483624c6fc46f7e79e99f
LeeDS <ehdtn3540@gmail.com>
2025-01-13 16:03:42 +0900

    블루프린트 생성 및 등록

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -3,8 +3,7 @@ from flask import Flask
 def create_app():
 	app = Flask(__name__)
 
-	@app.route('/')
-	def hello_pybo():
-		return 'Hello, Pybo!' # Hello Pybo! 를 출력
+	from .views import main_views
+	app.register_blueprint(main_views.bp)
 
 	return app
