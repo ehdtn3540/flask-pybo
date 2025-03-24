14feee15732f3ed97462940ec395d827a845ab3b
LeeDS <ehdtn3540@gmail.com>
2025-03-24 17:34:36 +0900

    Question 모델에 user_id 컬럼 추가하기

--- pybo/models.py
+++ pybo/models.py
@@ -1,10 +1,12 @@
 from pybo import db
 
 class Question(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    subject = db.Column(db.String(200), nullable=False)
-    content = db.Column(db.Text(), nullable=False)
-    create_date = db.Column(db.DateTime(), nullable=False)
+	id = db.Column(db.Integer, primary_key=True)
+	subject = db.Column(db.String(200), nullable=False)
+	content = db.Column(db.Text(), nullable=False)
+	create_date = db.Column(db.DateTime(), nullable=False)
+	user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
+	user = db.relationship('User', backref=db.backref('question_set'))
 
 class Answer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
